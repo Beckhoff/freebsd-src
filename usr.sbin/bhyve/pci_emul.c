d282e89b8a8c182e18940f5d40a111d202dd2506
Corvin Köhne <c.koehne@beckhoff.com>
2021-03-19 13:32:35 +0100

    bhyve/BAR: use physical lobits for passthru devices
    To ensure a correct behaviour of passthru devices, we need to tell the
    guest if a BAR uses prefetched memory or not.
    
    Signed-off-by: Corvin Köhne <c.koehne@beckhoff.com>
    Reviewed-by: Patrick Bruenn <p.bruenn@beckhoff.com>

--- usr.sbin/bhyve/pci_emul.c
+++ usr.sbin/bhyve/pci_emul.c
@@ -710,6 +710,13 @@ pci_emul_assign_bar(const struct pci_bar_allocation *const pci_bar)
 	pdi->pi_bar[idx].type = type;
 	pdi->pi_bar[idx].addr = addr;
 	pdi->pi_bar[idx].size = size;
+	/* passthru devices are using same lobits as physical device
+	 * they set this property
+	 */
+	if (pdi->pi_bar[idx].lobits != 0)
+		lobits = pdi->pi_bar[idx].lobits;
+	else
+		pdi->pi_bar[idx].lobits = lobits;
 
 	/* Initialize the BAR register in config space */
 	bar = (addr & mask) | lobits;
@@ -1919,7 +1926,7 @@ pci_cfgrw(struct vmctx *ctx, int vcpu, int in, int bus, int slot, int func,
 			case PCIBAR_IO:
 				addr = *eax & mask;
 				addr &= 0xffff;
-				bar = addr | PCIM_BAR_IO_SPACE;
+				bar = addr | pi->pi_bar[idx].lobits;
 				/*
 				 * Register the new BAR value for interception
 				 */
@@ -1930,7 +1937,7 @@ pci_cfgrw(struct vmctx *ctx, int vcpu, int in, int bus, int slot, int func,
 				break;
 			case PCIBAR_MEM32:
 				addr = bar = *eax & mask;
-				bar |= PCIM_BAR_MEM_SPACE | PCIM_BAR_MEM_32;
+				bar |= pi->pi_bar[idx].lobits;
 				if (addr != pi->pi_bar[idx].addr) {
 					update_bar_address(pi, addr, idx,
 							   PCIBAR_MEM32);
@@ -1938,8 +1945,7 @@ pci_cfgrw(struct vmctx *ctx, int vcpu, int in, int bus, int slot, int func,
 				break;
 			case PCIBAR_MEM64:
 				addr = bar = *eax & mask;
-				bar |= PCIM_BAR_MEM_SPACE | PCIM_BAR_MEM_64 |
-				       PCIM_BAR_MEM_PREFETCH;
+				bar |= pi->pi_bar[idx].lobits;
 				if (addr != (uint32_t)pi->pi_bar[idx].addr) {
 					update_bar_address(pi, addr, idx,
 							   PCIBAR_MEM64);
